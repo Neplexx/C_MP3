c52092f4b4de7574d795baf375a16573f96f6785
Neplexx <76035661+Neplexx@users.noreply.github.com>
2023-09-27 21:27:27 +0200

    Convertisseur mp3
    Ajout : miniature de la vidéo directement affectée au fichier mp3

--- Convertisseur mp3.py
+++ Convertisseur mp3.py
@@ -4,18 +4,16 @@
 
 #Note : entrer dans la variable "chemin" le chemin d'accès vers l'endroit où vous voulez que vos musiques soient téléchargées        
 
-import yt_dlp , re
-from mutagen.id3 import ID3, TPE1, TALB
-from pytube import YouTube
-
+import yt_dlp , re , requests , os
+from mutagen.id3 import ID3, TPE1, TALB , APIC
 chemin = r"C:\Users\detal\Desktop\C_MP3"
 
 def Nettoie(texte):
     return re.sub(r'[\/:*?"<>|]', '_', texte)
 
 backslash = "\\"
-#URLS = ['https://www.youtube.com/watch?v=bTx9bi-Gwts']
-URLS = [input("Lien youtube de la musique/playlist :\n\n")]
+URLS = ['https://www.youtube.com/watch?v=bTx9bi-Gwts']
+#URLS = [input("Lien youtube de la musique/playlist :\n\n")]
 
 
 ydl_opts = {
@@ -42,14 +40,34 @@ if 'album' in info_dict:
 else:
     musique_album = musique_nom
 
-#partie mutagen permettant de modifier les métadonnées du fichier mp3 pour inclure les valeurs 'album' et 'artiste'
+#partie recherche et téléchargement de l'image
+
+response = requests.get(URLS[0])
+if response.status_code == 200:
+    thumbnail_url = response.text.split('<meta property="og:image" content="')[1].split('"')[0]
+    response_thumbnail = requests.get(thumbnail_url)
+    if response_thumbnail.status_code == 200:
+        with open(chemin + backslash + Nettoie(info_dict['title']+".jpg"), "wb") as thumbnail_file:
+            thumbnail_file.write(response_thumbnail.content)
+
+#partie mutagen permettant de modifier les métadonnées du fichier mp3 pour inclure les valeurs 'album' et 'artiste' (au cas où j'oublie)
 
 fichier_mp3 = chemin + backslash + Nettoie(info_dict['title']) + '.mp3'
+image_mp3 = chemin + backslash + Nettoie(info_dict['title']) + '.jpg'
+
+with open(image_mp3, 'rb') as image_file:
+    image_data = image_file.read()
 
 audio = ID3(fichier_mp3)
 audio.add(TPE1(encoding=3, text=musique_auteur))
 audio.add(TALB(encoding=3, text=musique_album))
+apic = APIC(encoding=3, mime='image/jpeg', type=3, desc=u'Cover', data=image_data)
+audio.add(apic)
 audio.save()
+
+#partie supprimer les restes
+
+os.remove(chemin + backslash + Nettoie(info_dict['title']) + '.jpg')
 
 input("Appuyez sur Entrée pour quitter...")
 
